22290daceacaaef649f752bce9537bacdb777722
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-09 12:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.sort((a,b)=>{
+        return a-b;
+    })
+
+    let sm=0;
+    while(arr.length!=1){
+        let d = arr[0]+arr[1]
+        sm+=d;
+        arr.shift();
+        arr.shift();
+        arr.unshift(d);
+        arr.sort((a,b)=>{
+            return a-b;
+        })
+    }
+    return sm;
   
 }
 
